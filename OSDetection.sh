a668b75d7836b8af654d465154ba44e0d1f40bda
root <root@localhost.localdomain>
2018-03-02 16:50:42 -0500

    Added logic and got exact TTL number.  Bug testing, commenting, readme, and making sure objectives met are next.

--- OSDetection.sh
+++ OSDetection.sh
@@ -4,7 +4,19 @@ file=$1
 
 while read line
 	do
-		ping -c 1 $line >/dev/null
+		longTTL=$( ping -c 1 $line | grep ttl | cut -d " " -f6 )
+		shortTTL=$( echo $longTTL | cut -d "=" -f2 )
+#		echo "$longTTL"
+#		echo "$shortTT"
+		if  [ $shortTTL == 64 ]
+		then
+			echo "$line is a Linux OS."
+		elif [ $shortTTL == 128 ]
+		then
+			echo "$line is a Windows OS."
+		else
+			echo "$line is an unkown OS."
+		fi
 
 	done <$file
 
