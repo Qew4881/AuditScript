20784138033ee2a4502cfff701cb54af4a92dc5b
root <root@localhost.localdomain>
2018-03-02 17:10:37 -0500

    Project completed.  Finished and commented script with a test file and a README for very basic use instructions.

--- OSDetection.sh
+++ OSDetection.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-file=$1
+file=$1		#Read in command line argument
 
-while read line
-	do
+while read line		#Loop to read in file of pings
+	do		#then shorten it to just the TTL number.
 		longTTL=$( ping -c 1 $line | grep ttl | cut -d " " -f6 )
 		shortTTL=$( echo $longTTL | cut -d "=" -f2 )
 #		echo "$longTTL"
-#		echo "$shortTT"
-		if  [ $shortTTL == 64 ]
+#		echo "$shortTTL"
+		if [ -z "$shortTTL" ]	#Logic to determine OS or unreachable
+		then 
+			echo "$line is an unreachable device."
+		elif [ $shortTTL == 64 ]
 		then
 			echo "$line is a Linux OS."
 		elif [ $shortTTL == 128 ]
 		then
 			echo "$line is a Windows OS."
-		else
-			echo "$line is an unkown OS."
+		elif [ $shortTTL == 255 ]
+		then
+			echo "$line is a FreeBSD OS."
 		fi
 
 	done <$file
---
+++ README.txt
@@ -0,0 +1,13 @@
+OS detection Script
+Author: Quinn White
+Last Update: 3/2/2018
+
+Call file name and give it a text file with a list of IP addresses, with
+one IP address per line.  Below is an example of how to call this program.
+
+Make sure all files are in the same folder.
+
+Ex:
+
+./OSDetection.sh text.txt
+
